980e89389f372696ee4558d4984040f50c848183
b4n9z <b4n9z4tt3r5@gmail.com>
2024-11-10 14:26:32 +0700

    New Again

---
+++ src/main/java/io/github/b4n9z/deathPulse/DeathPulse.java
@@ -0,0 +1,17 @@
+package io.github.b4n9z.deathPulse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathPulse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
